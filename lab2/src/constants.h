7b1c7e49f3260c45e668e2cdd423d7cd213ab2f4
Vladimir ProvaLove <vladimir.provalov@emlid.com>
2018-11-05 13:55:00 +0300

    fixup! lab2: src: add constants

--- lab2/src/constants.h
+++ lab2/src/constants.h
@@ -1,19 +1,11 @@
-#include <linux/module.h>
-#include <linux/version.h>
-#include <linux/kernel.h>
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
 
-#define DRV_NAME "memes"
-#define DRV_LOG_DELIM ": "
 
-// General purpose log prefix
-#define DRV_LOG_NAME "driver"
-#define DRV_LOG(type, ctx, ...) \
-    do { \
-        printk(KERN_##type \
-               DRV_LOG_NAME \
-               DRV_LOG_DELIM \
-               ctx \
-               DRV_LOG_DELIM \
-               __VA_ARGS__); \
-    } while(0)
+#define DRV_OP_SUCCESS 0
+#define DRV_NSECTORS 10
+#define DRV_SECTOR_SZ 512
+#define DRV_MINORS 16
 
+
+#endif
