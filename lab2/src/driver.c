096637320729628911b2d36868f3c33ba82bd1eb
Vladimir ProvaLove <vladimir.provalov@emlid.com>
2018-11-05 13:58:25 +0300

    lab2: src: fixup driver.c

--- lab2/src/driver.c
+++ lab2/src/driver.c
@@ -4,25 +4,39 @@
 #include <linux/types.h>
 #include <linux/kdev_t.h>
 #include <linux/fs.h>
-#include <linux/device.h>
-#include <linux/slab.h>
-#include <linux/cdev.h>
-#include <linux/string.h>
-#include <linux/syscalls.h>
-#include <linux/buffer_head.h>
-#include <asm/uaccess.h>
+#include <linux/spinlock.h>
+
+#include "constants.h"
+#include "logging.h"
 
 
+struct drv_dev {
+    int sz;
+    uint8_t* vdisk;
+    spinlock_t spinlock;
+    struct request_queue *queue;
+    struct gendisk *gendisk;
+};
 
-static int __init drv_init(void)
+
+static int __init drv_init(void) 
 {
+    DRV_LOG_INIT(INFO, "Starting initialization\n");
+    int major = register_blkdev(15, DRV_NAME);
 
+    if (major <= 0) {
+        DRV_LOG_INIT(ERR, "Could not register block device\n");
+        goto err;
+    }
+
+undo_blkdev_reg:
+    unregister_blkdev(major, DRV_NAME);
+err:
+    return -EBUSY; 
 }
 
-static void __exit drv_exit(void)
-{
 
-}
+static void __exit drv_exit(void) { }
 
 
 MODULE_LICENSE("GPL");
