d15af0fe90d8e2c2a2b94357207e7aed21b9dfcb
Vladimir ProvaLove <vladimir.provalov@emlid.com>
2018-12-09 16:07:40 +0300

    lab3: add interface initialization

--- lab3/src/driver.c
+++ lab3/src/driver.c
@@ -18,11 +18,13 @@
 
 
 #define DRV_RES_SUCCESS 0
+#define DRV_RES_FAILURE -1
 #define DRV_TARGET_PORT 32
 
 
 static struct packet_type pack_type;
-
+static struct net_device * netdev = NULL;
+static struct net_device_stats netdev_statistics;
 
 //
 // Helpers
@@ -113,12 +115,109 @@ static void release_packet_interception(void)
 }
 
 
+//
+// Stubs
+//
+
+
+static int ndev_open(struct net_device * dev)
+{
+    DRV_LOG_CTX_SET("open");
+    LG_INF("Device opened");
+    return DRV_RES_SUCCESS;
+}
+
+
+static int ndev_stop(struct net_device * dev)
+{
+    DRV_LOG_CTX_SET("stop");
+    LG_INF("Device stopped");
+    return DRV_RES_SUCCESS;
+}
+
+
+static int ndev_hard_start_xmit(struct sk_buff * skb, struct net_device * dev)
+{
+    DRV_LOG_CTX_SET("xmit");
+    LG_INF("Start transmitting");
+    return DRV_RES_SUCCESS;
+}
+
+
+static void ndev_tx_timeout(struct net_device * dev)
+{
+    DRV_LOG_CTX_SET("timeout");
+    LG_INF("Timed out");
+}
+
+
+static struct net_device_stats * ndev_get_stats(struct net_device * dev)
+{
+    DRV_LOG_CTX_SET("device_stats")
+    LG_INF("Retrieving device stats");
+    return &netdev_statistics;
+}
+
+
+static int ndev_set_config(struct net_device * dev, struct ifmap * map)
+{
+    DRV_LOG_CTX_SET("config");
+    LG_INF("Setting new config");
+    return DRV_RES_SUCCESS;
+}
+
+
+static struct net_device_ops ndev_ops = {
+    .ndo_open = ndev_open,
+    .ndo_stop = ndev_stop,
+    .ndo_start_xmit = ndev_hard_start_xmit,
+    .ndo_tx_timeout = ndev_tx_timeout,
+    .ndo_get_stats = ndev_get_stats,
+    .ndo_set_config = ndev_set_config,
+};
+
+
+static int setup_network_interface(void)
+{
+    DRV_LOG_CTX_SET("setup_netwk")
+
+    netdev = alloc_etherdev(20);
+    if (netdev == NULL) {
+        LG_FAILED_TO("allocate ethernet device");
+        goto out;
+    }
+
+    netdev->netdev_ops = &ndev_ops;
+    if (register_netdev(netdev)) {
+        LG_FAILED_TO("register network device");
+        goto release_netdev;
+    }
+
+    return DRV_RES_SUCCESS;
+
+release_netdev:
+    free_netdev(netdev);
+out:
+    return DRV_RES_FAILURE;
+}
+
+
+static void release_network_interface(void)
+{
+    unregister_netdev(netdev);
+    free_netdev(netdev);
+}
+
+
 static int __init drv_init(void)
 {
     DRV_LOG_CTX_SET("drv_init");
     LG_INF("Initializing the module");
 
-    setup_packet_interception();
+    if (setup_packet_interception())
+        return DRV_RES_FAILURE;
+    if (setup_network_interface())
+        return DRV_RES_FAILURE;
     return DRV_RES_SUCCESS;
 }
 
@@ -128,6 +227,7 @@ static void __exit drv_exit(void)
     DRV_LOG_CTX_SET("drv_init");
     LG_INF("Cleaning up the module");
 
+    release_network_interface();
     release_packet_interception();
 }
 
